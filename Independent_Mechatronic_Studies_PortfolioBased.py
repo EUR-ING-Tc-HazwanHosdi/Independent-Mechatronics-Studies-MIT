c62141004cb41f4b52f759f1f3c424e93c6621ff
EUR-ING-Tc-HazwanHosdi <onezwan12@gmail.com>
2026-05-16 20:05:07 +0800

    Update Independent_Mechatronic_Studies_PortfolioBased.py

--- Independent_Mechatronic_Studies_PortfolioBased.py
+++ Independent_Mechatronic_Studies_PortfolioBased.py
@@ -586,7 +586,7 @@ elif menu == "Journal":
     
     for _, j in journal_df.iterrows():
         with st.container(border=True):
-            jh1, jh2, jh3 = st.columns([6, 8, 1])
+            jh1, jh2, jh3 = st.columns([6, 10, 2])
             jh2.subheader(j['title'])
             
             with jh2:
